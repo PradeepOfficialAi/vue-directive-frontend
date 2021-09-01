f064285ff34e3e6300ac364071218c839d9e1f4f
PradeepDrive <88648255+PradeepDrive@users.noreply.github.com>
2021-09-01 18:16:39 +0530

    First commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive('pradeep', {
+    bind(html_element, binding) {
+        html_element.style.color = binding.value
+        console.log("Hey Your Shit what doing", html_element, binding.modifiers);
+    }
+})
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
